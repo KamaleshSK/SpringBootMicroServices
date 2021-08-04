859abe59c5040dbce0fb90303f134188f7bbc9e1
therageofshiva <skamaleshfg@gmail.com>
2021-08-04 21:59:41 +0530

    Initial Commit of spring boot project and update gitignore

---
+++ src/test/java/com/plantplaces/demo/PlantPlacesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plantplaces.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantPlacesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
